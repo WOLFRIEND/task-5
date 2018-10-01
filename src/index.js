3d6d0564c6ad88003b50ab5e21482b2eee5233d9
WOLFRIEND <41586082+WOLFRIEND@users.noreply.github.com>
2018-10-01 20:44:07 +0400

    Adding files with configured Webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// CSS files import
+import './style.scss'
+
+// JS files import
+import test from './test'
+
+//Testing exported function
+test('Kokojambo');
---
+++ src/test.js
@@ -0,0 +1,4 @@
+//Export testing
+export default function test (name) {
+    console.log(`my name is ${name}`)
+}
